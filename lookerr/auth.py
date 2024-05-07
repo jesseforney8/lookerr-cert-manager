453b0008deaae2747fc1eb00b510747587c59e36
Jesse <jesseforney8@gmail.com>
2024-05-06 22:06:10 -0500

    fixed up navbar

--- lookerr/auth.py
+++ lookerr/auth.py
@@ -54,3 +54,9 @@ def sign_up():
                  
 
         return render_template("signup.html")
+
+@auth.route("/logout")
+@login_required
+def logout():
+     logout_user()
+     return redirect("/login")
