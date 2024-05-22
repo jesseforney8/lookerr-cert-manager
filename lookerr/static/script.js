7e7e19d1811de4a0c669b8da63e12d0ab6106249
Jesse <jesseforney8@gmail.com>
2024-05-21 21:23:16 -0500

    fixed radio button

--- lookerr/static/script.js
+++ lookerr/static/script.js
@@ -4,11 +4,23 @@ function add_devices(){
     let server_user = document.getElementById("server_user").value;
     let server_pass = document.getElementById("server_password").value;
     let filepath = document.getElementById("filepath").value;
+
+    if (document.getElementById("Windows").checked) {
+        var os = document.getElementById("Windows").value;
+
+
+    } else {
+        var os = document.getElementById("Linux").value;
+
+
+    };
     
+    
+    console.log(os)
 
     fetch("/devices", {
               method: "POST",
-              body: JSON.stringify({name: device_name, ip: device_ip, os: "dummy", server_user: server_user, server_pass: server_pass, file_path: filepath, device_cert: "dummy"}),
+              body: JSON.stringify({name: device_name, ip: device_ip, os: os, server_user: server_user, server_pass: server_pass, file_path: filepath, device_cert: "dummy"}),
             }).then((_res) => {
                 window.location.href = "/devices";
             });
